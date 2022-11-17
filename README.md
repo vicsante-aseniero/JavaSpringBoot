5c2c19559470c7dd866ca9a0bb8488725f9471c1
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2022-11-17 20:51:38 +0800

    Adding "Hello World"

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Learning Java Spring Boot
 Using "Spring Initializr" to generate Spring Boot Project.
 https://start.spring.io
 
+Using Java 19 JDK and MySQL Database for the REST API.
+
+Layers are as follows:
+- REST API
+- Service Layer (where Business Logic resides)
+- Data Layer (where models, db, and CRUD resides)
+
+
+
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+import java.util.List;
+
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> hello()
+	{
+		return List.of("Hello", "World");
+	}
 }
