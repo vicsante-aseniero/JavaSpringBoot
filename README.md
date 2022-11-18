7639b57270ca749f9c960923c160c826854fc901
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2022-11-18 13:45:17 +0800

    Adding Student Model and expose it as HTTPGet Endpoint JSON result

--- README.md
+++ README.md
@@ -2,14 +2,18 @@
 Learning Java Spring Boot
 
 Using "Spring Initializr" to generate Spring Boot Project.
-https://start.spring.io
+
+URL: https://start.spring.io
 
 Using Java 19 JDK and MySQL Database for the REST API.
 
 Layers are as follows:
 - REST API
 - Service Layer (where Business Logic resides)
-- Data Layer (where models, db, and CRUD resides)
+- Data Access Layer (where models, db, and CRUD resides)
+- Database (i.e. MySQL, MariaDB, PostgreSQL, MongoDB)
+
+
 
 
 
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
@@ -17,8 +20,16 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello()
+	public List<Student> hello()
 	{
-		return List.of("Hello", "World");
+		return List.of(
+				new Student(
+						1L,
+						"Sante",
+						"sante@my.org",
+						LocalDate.of(2020, Month.NOVEMBER, 18),
+						50
+				)
+		);
 	}
 }
