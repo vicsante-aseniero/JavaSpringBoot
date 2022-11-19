52baa1373a8307ab816f2e34f680c2a8d74c1a44
Vicsante Tangpos Aseniero <vaseniero@gmail.com>
2022-11-19 16:27:35 +0800

    Adding connection configuration in application.properties

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,8 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 public class DemoApplication {
